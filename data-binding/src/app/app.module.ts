2f24ade3b9a0a3cb97a1250db1783c13bae2244f
Fabio <fabio.devx@gmail.com>
2019-02-02 19:22:24 -0200

    Event Binding, Two way data binding and Input and Output properties labs

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { MeuFormModule } from './meu-form/meu-form.module';
+import { InputPropertyComponent } from './input-property/input-property.component';
+import { OutputPropertiesComponent } from './output-properties/output-properties.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    InputPropertyComponent,
+    OutputPropertiesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MeuFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -11,17 +11,50 @@ export class DataBindingComponent implements OnInit {
   urlImagem: string = 'http://lorempixel.com/400/200/nature';
   cursoAngular: boolean = true;
 
+  valorAtual: string;
+  valorSalvo: string;
+
+  isMouseOver: boolean;
+
+  nome: string = 'abc';
+  
+  nomeDoCurso: string = 'Angular';
+
+  valorInicial: number = 15;
+
   constructor() { }
 
   
   ngOnInit() {
+    this.nome = 'abc';
   }
 
   getValor() {
     return 1;
   }
 
+  botaoClicado(){
+    alert('botaClicado')
+  }
+
+  eventoTecla(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value
+    console.info(this.valorAtual);
+  }
+
+  setValor(valor) {
+    this.valorSalvo = valor;
+  }
+
   getCurtirCurso() {
     return true;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  onMudouValor(evento) {
+    console.log(evento);
   }
 }
