eed41c4030dbd49943376bd8401b12f2c8b4f797
Fabio <fabio.devx@gmail.com>
2019-02-02 20:41:23 -0200

    ViewChild and DOM manipulation lab

--- data-binding/src/app/output-properties/output-properties.component.ts
+++ data-binding/src/app/output-properties/output-properties.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -10,18 +10,21 @@ export class OutputPropertiesComponent implements OnInit {
   @Input() valor: number = 0;
   @Output() mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput') campoValorInput: ElementRef
   constructor() { }
 
   ngOnInit() {
   }
 
   incrementa() {
-    this.valor++;
+    this.campoValorInput.nativeElement.value++;
+    this.valor = this.campoValorInput.nativeElement.value;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
   decrementa() {
-    this.valor--;
+    this.campoValorInput.nativeElement.value--;
+    this.valor = this.campoValorInput.nativeElement.value;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
