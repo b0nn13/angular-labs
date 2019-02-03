272984f2ef438f9c560832973df11fe24a92ab08
Fabio <fabio.devx@gmail.com>
2019-02-03 00:50:27 -0200

    ngIf and ngSwitch directives lab

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DiretivaNgifComponent,
+    DiretivaNgswitchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgifComponent implements OnInit {
 
+  cursos: string[] = ['Angular'];
+  mostrarCursos: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onMostrarCurso() {
+    this.mostrarCursos = !this.mostrarCursos;
   }
 
 }
