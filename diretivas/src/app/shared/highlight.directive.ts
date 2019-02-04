ea0e8e5ea9c47eb5c62f6ca6967ec5e4e0baa759
Fabio <fabio.devx@gmail.com>
2019-02-03 23:27:00 -0200

    Directive highlight with input colors

---
+++ diretivas/src/app/shared/highlight.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, HostListener, HostBinding, Input, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[highlight]'
+})
+export class HighlightDirective implements OnInit {
+
+  @HostListener('mouseenter') onmouseOver() {
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') onmouseOut() {
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
+  @Input() defaultColor: string = 'white';
+  @Input() highlightColor: string = 'yellow';
+
+  constructor() { 
+
+  }
+
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor;
+  }
+
+}
